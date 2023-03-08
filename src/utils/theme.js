44da67e9fd7fbf7e812211f7d787a494398e6aad
Iuliia Sokol <avare779@gmail.com>
2023-03-08 12:34:22 +0200

    added theme

---
+++ src/utils/theme.js
@@ -0,0 +1,52 @@
+export const theme = {
+  breakpoints: ['375px', '768px', '1440px'],
+  colors: {
+    mainDark: '#23262A',
+    secondaryDark: '#3E4462',
+    mainBlack: '#000000',
+    mainGrey: '##7E7E7E',
+    secondaryGrey: '#656565',
+    disabledGrey: '#BDBDBD',
+    mainLight: '#FFFFFF',
+    mainAccent: '#8BAA36',
+    secondaryAccent: '#EBF3D4',
+
+    btnTextLight: '#FAFAFA',
+    sectionHeaderDark: '#001833',
+
+    mainGreyBg: '#D9D9D9',
+    btnHoverBg: '#22252A',
+    iconHover: ' #1E1F28',
+    switchBg: '#EFEFEF',
+  },
+  fonts: {
+    main: 'Poppins, sans-serif',
+  },
+  fontWeights: [400, 500, 600, 700],
+  lineHeights: {
+    subheader: '1.16',
+    content: '1.33',
+    extraContent: '1.29',
+    btnText: '1.12',
+  },
+  letterSpacings: {
+    subheader: '-0.24px',
+    content: '-0.02em',
+  },
+
+  borders: {
+    btnStandart: '2px solid #FAFAFA',
+    btnHover: '2px solid #8BAA36',
+  },
+  radii: {
+    btnStandart: '6px',
+    btnIcon: '4px',
+    logo: '12px',
+    image: '8px',
+    switch: '50px',
+    circle: '50%',
+  },
+  transitions: {
+    main: 'all 250ms cubic-bezier(0.4, 0, 0.2, 1)',
+  },
+};
