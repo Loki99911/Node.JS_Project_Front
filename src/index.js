5db2e7669b0f26c791d40e8d97a2ca4e9b54b410
MarianaDobrivska <dobrivska.marry@gmail.com>
2023-03-09 00:09:37 +0200

    add store and persist gate into index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
+
 import { App } from 'components/App';
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './utils/theme';
+import { store, persistor } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<BrowserRouter>
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
-</BrowserRouter>
+  <BrowserRouter>
+    <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
+        </PersistGate>
+      </Provider>
+    </React.StrictMode>
+  </BrowserRouter>
 );
