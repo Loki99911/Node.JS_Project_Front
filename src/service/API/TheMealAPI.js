44e760c7a2714bef0e03bc76be3697a06faf0aa6
MarianaDobrivska <dobrivska.marry@gmail.com>
2023-03-09 00:14:48 +0200

    preparation for recipes operations

---
+++ src/service/API/TheMealAPI.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://soyummy.onrender.com';
+
+export const getCategoryListAPI = () => {};
+
+export const getAllCategoriesAndRecipesAPI = () => {};
+
+export const getRecipesByCategoryAPI = category => {};
+
+export const getOneRecipeByIdAPI = id => {};
+
+export const getPopularRecipesAPI = () => {};
+
+export const getAllIngredientsAPI = () => {};
