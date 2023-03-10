5418981beaf0aa57b727aa7fd43a7ffac5e03710
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2023-03-10 18:06:37 +0200

    useMedia on Auth

--- src/components/AuthPages/AuthPages.jsx
+++ src/components/AuthPages/AuthPages.jsx
@@ -1,12 +1,18 @@
 import { BoxForAuth, AuthImage, BoxForImage } from './style';
-import authPana from 'images/desktop/AuthPanaDes.png';
+import authPanaDes from 'images/desktop/AuthPanaDes.png';
+import authPanaTab from 'images/tablet/AuthPanaTab-min.png';
+import authPanaMob from 'images/mobile/AuthPanaMob-min.png';
+import { useMediaRules } from 'MediaRules/MediaRules';
 
 const AuthPages = ({ children }) => {
+  const { isMobile, isDesktop, isTablet } = useMediaRules();
   return (
     <>
       <BoxForAuth>
         <BoxForImage>
-          <AuthImage src={authPana} alt="pana" />
+          {isMobile && <AuthImage src={authPanaMob} alt="pana" />}
+          {isTablet && <AuthImage src={authPanaTab} alt="pana" />}
+          {isDesktop && <AuthImage src={authPanaDes} alt="pana" />}
         </BoxForImage>
         {children}
       </BoxForAuth>
