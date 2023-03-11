6b22d6ddba3958598b28c61ea3d374eacce12fe1
Andrii Shpak <SHPAKK2@GMAIL.COM>
2023-03-11 23:23:24 +0200

    23

--- src/components/RecipeInngredientsList/RecipeInngredientsList.jsx
+++ src/components/RecipeInngredientsList/RecipeInngredientsList.jsx
@@ -3,8 +3,10 @@ import { InngredientsWrapper } from './RecipeInngredientsList.styled';
 
 import { getIngredients } from 'redux/ingredients/ingredientsSelectors';
 import { useSelector } from 'react-redux';
+// import { useState } from 'react';
 
 const RecipeInngredientsList = ({ ingredients }) => {
+  // const [ingID, setIngID] = useState(false);
   const allOfIngredients = useSelector(getIngredients);
 
   function getIngDescription(ingName) {
@@ -13,11 +15,23 @@ const RecipeInngredientsList = ({ ingredients }) => {
         ing.strIngredient ===
         ingName.replace(ingName[0], ingName[0].toUpperCase())
     );
-    // if (ing && ing.strDescription !== null) {
-    //   ing.strDescription === "";
-    //   return ing;
-    // }
     return ing;
+  }
+
+  function getDescr(name) {
+    const descr = getIngDescription(name);
+    if (!descr) {
+      return '';
+    }
+    return descr.strDescription;
+  }
+
+  function getID(name) {
+    const descr = getIngDescription(name);
+    if (!descr) {
+      return '';
+    }
+    return descr.idIngredient;
   }
 
   return (
@@ -28,11 +42,11 @@ const RecipeInngredientsList = ({ ingredients }) => {
             image={
               'https://i.pinimg.com/564x/e9/2b/df/e92bdfc88c52c9600a9f545fbc443d4d.jpg'
             }
-            idIngredient={getIngDescription(inngredient.name).idIngredient}
             strIngredient={inngredient.name}
             key={inngredient.id}
             weight={inngredient.number}
-            strDescription={getIngDescription(inngredient.name).strDescription}
+            strDescription={getDescr(inngredient.name)}
+            idIngredient={getID(inngredient.name)}
           />
         ))}
       </InngredientsWrapper>
--- src/components/RecipePageHero/RecipePageHero.jsx
+++ src/components/RecipePageHero/RecipePageHero.jsx
@@ -1,11 +1,28 @@
 import { RecipeHeroConteiner, HeroTitle } from './RecipePageHero.styled';
 import { ButtonSkew } from 'components/ButtonSkew/ButtonSkew';
-// import { useEffect, useState } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { getFavorite } from 'redux/ownRecipes/ownRecipesOperations';
-// import { getFavoriteRecipes } from 'redux/ownRecipes/ownRecipesSelectors';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getFavorite } from 'redux/ownRecipes/ownRecipesOperations';
+import { getFavoriteRecipes } from 'redux/ownRecipes/ownRecipesSelectors';
 
 const RecipePageHero = ({ meal }) => {
+  // const [btnText, setBtnText] = useState(false);
+  const dispatcher = useDispatch();
+  const array = useSelector(getFavoriteRecipes);
+
+  function getIngDescription(ingName) {
+    const ing = array.some(
+      ing =>
+        ing.strMeal === ingName.replace(ingName[0], ingName[0].toUpperCase())
+    );
+    console.log(ing);
+    return ing;
+  }
+
+  useEffect(() => {
+    dispatcher(getFavorite());
+  }, [dispatcher]);
+  console.log(array);
   // const [Meal, setMeal] = useState();
 
   // useEffect(() => {
@@ -18,12 +35,21 @@ const RecipePageHero = ({ meal }) => {
     <>
       <RecipeHeroConteiner>
         <HeroTitle>{meal}</HeroTitle>
-        <ButtonSkew
-          type="button"
-          text="Add to favorite recipes"
-          styled="other"
-          location="recipes"
-        />
+        {getIngDescription(meal) ? (
+          <ButtonSkew
+            type="button"
+            text="delete from favorite recipes"
+            styled="other"
+            location="recipes"
+          />
+        ) : (
+          <ButtonSkew
+            type="button"
+            text="add to favorite recipes"
+            styled="other"
+            location="recipes"
+          />
+        )}
       </RecipeHeroConteiner>
     </>
   );
