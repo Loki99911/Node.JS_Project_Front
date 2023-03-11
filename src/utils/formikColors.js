02f8e4cf0f8f469c38f57fa7e248289ac6117833
Iuliia Sokol <avare779@gmail.com>
2023-03-11 12:32:37 +0200

    formik added

--- src/utils/formikColors.js
+++ src/utils/formikColors.js
@@ -1,11 +1,7 @@
 export const getColor = (errors, values) => {
-  return values
-    ? (errors && '#E74A3B') || '#3CBC81'
-    : 'rgba(255, 255, 255, 0.8)';
+  return values ? (errors && '#E74A3B') || '#3CBC81' : '#C4C4C4';
 };
 
 export const getColorBorder = (errors, values) => {
-  return values
-    ? (errors && '#E74A3B') || '#3CBC81'
-    : 'rgba(255, 255, 255, 0.3)';
+  return values ? (errors && '#E74A3B') || '#3CBC81' : '#C4C4C4';
 };
