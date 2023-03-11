d55b9756571992d6251bdfeea18dab3a042d63dc
Iuliia Sokol <avare779@gmail.com>
2023-03-11 12:49:56 +0200

    updated formik colors func

--- src/utils/formikColors.js
+++ src/utils/formikColors.js
@@ -1,7 +1,15 @@
-export const getColor = (errors, values) => {
-  return values ? (errors && '#E74A3B') || '#3CBC81' : '#C4C4C4';
+export const getColor = (
+  errors,
+  values,
+  defaultColor = 'rgba(255, 255, 255, 0.8)'
+) => {
+  return values ? (errors && '#E74A3B') || '#3CBC81' : defaultColor;
 };
 
-export const getColorBorder = (errors, values) => {
-  return values ? (errors && '#E74A3B') || '#3CBC81' : '#C4C4C4';
+export const getColorBorder = (
+  errors,
+  values,
+  defaultColor = 'rgba(255, 255, 255, 0.8)'
+) => {
+  return values ? (errors && '#E74A3B') || '#3CBC81' : defaultColor;
 };
