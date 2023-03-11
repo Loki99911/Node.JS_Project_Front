cd5e0c8b9525978375f267bdeb8c5aa8a3f31569
AlenaUshakova <alenushakova@gmail.com>
2023-03-11 14:41:44 +0200

    fixed routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,20 +8,20 @@ import Register from 'pages/Register/Register';
 import SearchIngredients from 'pages/SearchIngredients/SearchIngredients';
 import ShopingList from 'pages/ShoppingList/ShoppingList';
 import Signin from 'pages/Signin/Signin';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout/SharedLayout';
 import { PrivateRoute, PublicRoute } from 'service/routes';
-import { getIsLoggedIn } from '../redux/auth/authSelectors';
+// import { getIsLoggedIn } from '../redux/auth/authSelectors';
 import { getIngredients } from 'redux/ingredients/ingredientsSelectors';
 import { useSelector, useDispatch } from 'react-redux';
 import CategoriesByName from 'pages/CategoriesByName/CategoriesByName';
 import MainPage from 'pages/MainPage/MainPage';
-// import Error from 'pages/Error/Error';
+import Error from 'pages/Error/Error';
 import { useEffect } from 'react';
 import { getAllIngredients } from 'redux/ingredients/ingredientsOperations';
 
 export const App = () => {
-  const isUserLogin = useSelector(getIsLoggedIn);
+  // const isUserLogin = useSelector(getIsLoggedIn);
   const ingredients = useSelector(getIngredients);
   const dispatcher = useDispatch();
   // const isUserLogin = true;
@@ -33,7 +33,7 @@ export const App = () => {
 
   return (
     <Routes>
-      {!isUserLogin ? (
+      {/* {!isUserLogin ? ( */}
         <>
           <Route
             path="/"
@@ -61,7 +61,7 @@ export const App = () => {
           />
           {/* <Route path="*" element={<Error />} /> */}
         </>
-      ) : (
+      {/* ) : ( */}
         <Route
           path="/"
           element={
@@ -80,10 +80,10 @@ export const App = () => {
           <Route path="/shopping-list" element={<ShopingList />} />
           <Route path="/search" element={<SearchIngredients />} />
           <Route path="/recipe/:recipeId" element={<Recipe />} />
-          {/* <Route path="*" element={<Error />} /> */}
+          <Route path="*" element={<Error />} />
         </Route>
-      )}
-      <Route path="*" element={<Navigate to='/' />} />
+      {/* )} */}
+      {/* <Route path="*" element={<Navigate to='/main' />} /> */}
     </Routes>
   );
 };
