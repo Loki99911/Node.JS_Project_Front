b3c9b5f70f43fde26f130bdac2f11247a0dc4c6e
Iuliia Sokol <avare779@gmail.com>
2023-03-12 13:26:21 +0200

    theme toggling added

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -59,3 +59,65 @@ export const theme = {
     main: 'all 250ms cubic-bezier(0.4, 0, 0.2, 1)',
   },
 };
+
+export const darkTheme = {
+  breakpoints: ['375px', '768px', '1440px'],
+  colors: {
+    mainDark: '#23262A',
+    secondaryDark: '#3E4462',
+    mainBlack: '#000000',
+    mainGrey: '##7E7E7E',
+    secondaryGrey: '#656565',
+    disabledGrey: '#BDBDBD',
+    mainLight: '#FFFFFF',
+    mainAccent: '#8BAA36',
+    secondaryAccent: '#EBF3D4',
+
+    bodyBg: '#23262A',
+    mainBg: '#ECECEC',
+    btnTextLight: '#FAFAFA',
+    sectionHeaderDark: '#001833',
+
+    mainGreyBg: '#D9D9D9',
+    secondaryGreyBg: '#C4C4C4',
+    btnHoverBg: '#22252A',
+    iconHover: ' #1E1F28',
+    switchBg: '#EFEFEF',
+  },
+  fonts: {
+    main: 'Poppins, sans-serif',
+  },
+  fontWeights: [400, 500, 600, 700],
+  lineHeights: {
+    subheader: '1',
+    content: '1.33',
+    extraContent: '1.29',
+    btnText: '1.12',
+    description: '1.25',
+    time: '1.43',
+    headerLinks: '1.6',
+    userLink: '1.7',
+  },
+  letterSpacings: {
+    subheader: '-0.24px',
+    content: '-0.02em',
+  },
+
+  borders: {
+    btnStandart: '2px solid #FAFAFA',
+    btnHover: '2px solid #8BAA36',
+  },
+  radii: {
+    btnStandart: '6px',
+    btnIcon: '4px',
+    logo: '12px',
+    image: '8px',
+    switch: '50px',
+    pagination: '26px',
+    modal: '24px',
+    circle: '50%',
+  },
+  transitions: {
+    main: 'all 250ms cubic-bezier(0.4, 0, 0.2, 1)',
+  },
+};
