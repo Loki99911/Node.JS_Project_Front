c20259d7438989b17c26562e2a3783dc7aaff901
Loki99911 <nikita_kresik@i.ua>
2023-03-13 19:45:47 +0200

    1

--- src/redux/theme/themeSlice.jsx
+++ src/redux/theme/themeSlice.jsx
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const themeSlice = createSlice({
   name: 'theme',
   initialState: {
-    theme: 'light',
+    theme: {mode: 'light'},
   },
   reducers: {
     setTheme: (_, { payload }) => ({
