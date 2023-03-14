1d9419832d2a07a0b58f6550c7e899a92cfd74a1
Loki99911 <nikita_kresik@i.ua>
2023-03-14 19:37:19 +0200

    13

--- src/components/Search/SearchTypeSelector/SearchTypeSelector.jsx
+++ src/components/Search/SearchTypeSelector/SearchTypeSelector.jsx
@@ -2,80 +2,80 @@ import Select from 'react-select';
 import { SelectCon } from './SearchTypeSelector.styled';
 import { useEffect, useState } from 'react';
 
-const customStyles = {
-  control: (baseStyles, state) => ({
-    ...baseStyles,
-    backgroundColor: '#D9D9D9',
-    borderColor: state.isFocused ? 'transparent' : '#D9D9D9',
-    boxShadow: state.isFocused ? '0 0 0 1px transparent' : null,
-    borderRadius: '6px',
-    fontFamily: 'Poppins',
-    fontStyle: 'normal',
-    fontWeight: '400',
-    fontSize: '12px',
-    lineHeight: '18px',
-    letterSpacing: '-0.02em',
-    textAlign: 'left',
-    // opacity: '0.5',
-    width: '146px',
-    height: '34px',
-    border: 'unset',
-    '@media (min-width: 768px)': {
-      fontSize: '14px',
-      lineHeight: '21px',
-      width: '175px',
-      height: '41px',
-    },
-    outline: state.isSelected && state.isFocused ? 'grey' : null,
-  }),
-  contanier: (baseStyles, state) => ({
-    ...baseStyles,
-    outline: state.isSelected && state.isFocused ? 'grey' : null,
-    boxShadow: state.isFocused ? '0 0 0 1px transparent' : null,
-    color: 'rgba(0, 0, 0, 0.5) !important',
-  }),
-  menuList: (baseStyles, state) => ({
-    ...baseStyles,
-    fontFamily: 'Poppins',
-    fontStyle: 'normal',
-    fontWeight: '400',
-    fontSize: '12px',
-    lineHeight: '18px',
-    '@media (min-width: 768px)': {
-      fontSize: '14px',
-      lineHeight: '21px',
-    },
-    letterSpacing: '-0.02em',
-    textAlign: 'left',
-    // opacity: '0.5',
-    color: 'rgba(0, 0, 0, 0.5) !important',
-  }),
+// const customStyles = {
+//   control: (baseStyles, state) => ({
+//     ...baseStyles,
+//     backgroundColor: '#D9D9D9',
+//     borderColor: state.isFocused ? 'transparent' : '#D9D9D9',
+//     boxShadow: state.isFocused ? '0 0 0 1px transparent' : null,
+//     borderRadius: '6px',
+//     fontFamily: 'Poppins',
+//     fontStyle: 'normal',
+//     fontWeight: '400',
+//     fontSize: '12px',
+//     lineHeight: '18px',
+//     letterSpacing: '-0.02em',
+//     textAlign: 'left',
+//     // opacity: '0.5',
+//     width: '146px',
+//     height: '34px',
+//     border: 'unset',
+//     '@media (min-width: 768px)': {
+//       fontSize: '14px',
+//       lineHeight: '21px',
+//       width: '175px',
+//       height: '41px',
+//     },
+//     outline: state.isSelected && state.isFocused ? 'grey' : null,
+//   }),
+//   contanier: (baseStyles, state) => ({
+//     ...baseStyles,
+//     outline: state.isSelected && state.isFocused ? 'grey' : null,
+//     boxShadow: state.isFocused ? '0 0 0 1px transparent' : null,
+//     color: 'rgba(0, 0, 0, 0.5) !important',
+//   }),
+//   menuList: (baseStyles, state) => ({
+//     ...baseStyles,
+//     fontFamily: 'Poppins',
+//     fontStyle: 'normal',
+//     fontWeight: '400',
+//     fontSize: '12px',
+//     lineHeight: '18px',
+//     '@media (min-width: 768px)': {
+//       fontSize: '14px',
+//       lineHeight: '21px',
+//     },
+//     letterSpacing: '-0.02em',
+//     textAlign: 'left',
+//     // opacity: '0.5',
+//     color: 'rgba(0, 0, 0, 0.5) !important',
+//   }),
 
-  option: (baseStyles, state) => ({
-    ...baseStyles,
-    backgroundColor: state.isSelected ? '#D9D9D9' : baseStyles.backgroundColor,
-    color: state.isSelected
-      ? 'rgba(0, 0, 0, 0.5) !important'
-      : baseStyles.color,
-  }),
-  dropdownIndicator: baseStyles => ({
-    ...baseStyles,
-    color: '#8BAA36',
-    // opacity: 1,
-  }),
-  indicatorSeparator: baseStyles => ({
-    ...baseStyles,
-    opacity: 0,
-  }),
-  menu: baseStyles => ({
-    ...baseStyles,
-    margin: 0,
-  }),
-  placeholder: baseStyles => ({
-    ...baseStyles,
-    color: 'rgba(0, 0, 0, 0.5) !important',
-  }),
-};
+//   option: (baseStyles, state) => ({
+//     ...baseStyles,
+//     backgroundColor: state.isSelected ? '#D9D9D9' : baseStyles.backgroundColor,
+//     color: state.isSelected
+//       ? 'rgba(0, 0, 0, 0.5) !important'
+//       : baseStyles.color,
+//   }),
+//   dropdownIndicator: baseStyles => ({
+//     ...baseStyles,
+//     color: '#8BAA36',
+//     // opacity: 1,
+//   }),
+//   indicatorSeparator: baseStyles => ({
+//     ...baseStyles,
+//     opacity: 0,
+//   }),
+//   menu: baseStyles => ({
+//     ...baseStyles,
+//     margin: 0,
+//   }),
+//   placeholder: baseStyles => ({
+//     ...baseStyles,
+//     color: 'rgba(0, 0, 0, 0.5) !important',
+//   }),
+// };
 
 export const SearchTypeSelector = ({ typeSubmit, startType }) => {
   const [selectedOption, setSelectedOption] = useState({
@@ -100,7 +100,7 @@ export const SearchTypeSelector = ({ typeSubmit, startType }) => {
         onChange={setSelectedOption}
         options={options}
         isSearchable={false}
-        styles={customStyles}
+        // styles={customStyles}
       />
     </SelectCon>
   );
