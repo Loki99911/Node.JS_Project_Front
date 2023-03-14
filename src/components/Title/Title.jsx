49abb8a77474be62777ebd6ee5875e537a618ebc
Iuliia Sokol <avare779@gmail.com>
2023-03-14 10:47:09 +0200

    mode line rewritten

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -3,10 +3,10 @@ import { getMode } from 'redux/theme/themeSelector';
 import { TitleConteiner } from './Title.styled';
 
 export const Title = ({ children }) => {
-  const mode = useSelector(getMode);
+  const { mode } = useSelector(getMode);
 
   return (
-    <TitleConteiner mode={mode.mode}>
+    <TitleConteiner mode={mode}>
       <h2>{children}</h2>
     </TitleConteiner>
   );
