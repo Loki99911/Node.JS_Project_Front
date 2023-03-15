3697bd237d3afba951ddd3e7f10bb868455f14b4
Nikita Kresik <51223547+Loki99911@users.noreply.github.com>
2023-03-15 17:39:39 +0200

    Update Favorites.jsx

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   getFavoriteRecipes,
