3320acc4e44ce99a976f07d5ae8572dad3f9f530
Andrii Shpak <SHPAKK2@GMAIL.COM>
2023-03-15 18:05:22 +0200

    upd
    changed according to new object

--- src/components/RecipeInngredientsList/RecipeInngredientsList.jsx
+++ src/components/RecipeInngredientsList/RecipeInngredientsList.jsx
@@ -3,48 +3,21 @@ import { InngredientsWrapper } from './RecipeInngredientsList.styled';
 
 import { getIngredients } from 'redux/ingredients/ingredientsSelectors';
 import { useSelector } from 'react-redux';
-// import { useState } from 'react';
 
 const RecipeInngredientsList = ({ ingredients }) => {
-  // const [ingID, setIngID] = useState(false);
   const allOfIngredients = useSelector(getIngredients);
-
-  function getIngDescription(ingName) {
-    const ing = allOfIngredients.find(
-      ing =>
-        ing.strIngredient ===
-        ingName.replace(ingName[0], ingName[0].toUpperCase())
-    );
-    return ing;
-  }
-
-  function getDescr(name) {
-    const descr = getIngDescription(name);
-    if (!descr) {
-      return '';
-    }
-    return descr.strDescription;
-  }
-
-  function getID(name) {
-    const descr = getIngDescription(name);
-    if (!descr) {
-      return '';
-    }
-    return descr.idIngredient;
-  }
 
   return (
     allOfIngredients && (
       <InngredientsWrapper>
         {ingredients.map(inngredient => (
           <RecipeInngredientsItem
-            image={inngredient.img}
-            strIngredient={inngredient.name}
+            image={inngredient.imgURL}
+            strIngredient={inngredient.ingredient}
             key={inngredient.id}
-            weight={inngredient.number}
-            strDescription={getDescr(inngredient.name)}
-            idIngredient={getID(inngredient.name)}
+            weight={inngredient.qty}
+            strDescription={inngredient.description}
+            idIngredient={inngredient.id}
           />
         ))}
       </InngredientsWrapper>
