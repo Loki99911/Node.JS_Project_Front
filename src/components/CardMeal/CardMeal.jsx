b989a3904cd65854f06b44c990a037c6dc3a23e3
Iuliia Sokol <avare779@gmail.com>
2023-03-17 17:17:39 +0200

    fixed tooltip display

--- src/components/CardMeal/CardMeal.jsx
+++ src/components/CardMeal/CardMeal.jsx
@@ -1,28 +1,33 @@
-import { Link } from "react-router-dom"
-import { Tooltip } from "react-tooltip"
-import { CardImg, CardTitle, CardDish } from './CardMeal.styled';
+import { Link } from 'react-router-dom';
+import { Tooltip } from 'react-tooltip';
+import {
+  CardImg,
+  CardTitle,
+  CardDish,
+  TooltipWrapper,
+} from './CardMeal.styled';
 import NoImage from '../../images/default.jpg';
 
-
-
 export const CardMeal = ({ meal }) => {
-    return (
-        <CardDish>
-            <Link to={`/recipe/${meal.idMeal}`}>
-                <CardImg
-                    src={meal.strMealThumb ? meal.strMealThumb : { NoImage }}
-                    alt={meal.strMeal}
-                />
-                <CardTitle data-tooltip-id={meal.idMeal}>{meal.strMeal}</CardTitle>
-                {meal.strMeal.length > 30 && (
-                    <Tooltip
-                        id={meal.idMeal}
-                        content={meal.strMeal}
-                        place="bottom"
-                        noArrow="false"
-                    />
-                )}
-            </Link>
-        </CardDish>
-    );
-}
+  return (
+    <CardDish>
+      <Link to={`/recipe/${meal.idMeal}`}>
+        <CardImg
+          src={meal.strMealThumb ? meal.strMealThumb : { NoImage }}
+          alt={meal.strMeal}
+        />
+        <CardTitle data-tooltip-id={meal.idMeal}>{meal.strMeal}</CardTitle>
+        {meal.strMeal.length > 30 && (
+          <TooltipWrapper>
+            <Tooltip
+              id={meal.idMeal}
+              content={meal.strMeal}
+              place="bottom"
+              noArrow="false"
+            />
+          </TooltipWrapper>
+        )}
+      </Link>
+    </CardDish>
+  );
+};
