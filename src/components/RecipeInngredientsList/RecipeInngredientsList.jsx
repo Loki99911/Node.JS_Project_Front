3e5a388f9ec2ca2ead2a69f7968e3221b38d0f81
Andrii Shpak <SHPAKK2@GMAIL.COM>
2023-03-20 13:28:14 +0200

    message

--- src/components/RecipeInngredientsList/RecipeInngredientsList.jsx
+++ src/components/RecipeInngredientsList/RecipeInngredientsList.jsx
@@ -4,12 +4,15 @@ import { InngredientsWrapper } from './RecipeInngredientsList.styled';
 import { getIngredients } from 'redux/ingredients/ingredientsSelectors';
 import { useSelector, useDispatch } from 'react-redux';
 import { nanoid } from 'nanoid';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getShoppingList } from 'redux/ingredients/ingredientsSelectors.js';
 import { getShoppingIngredient } from 'redux/ingredients/ingredientsOperations.js';
+import { MotivatedModal } from 'components/MotivatedModal/MotivatedModal';
 
 const RecipeInngredientsList = ({ ingredients, recipeId }) => {
   const allOfIngredients = useSelector(getIngredients);
+
+  const [triger, setTriger] = useState(0);
 
   const dispatcher = useDispatch();
   const list = useSelector(getShoppingList);
@@ -25,9 +28,16 @@ const RecipeInngredientsList = ({ ingredients, recipeId }) => {
     return false;
   }
 
+  useEffect(() => {
+    if (list.length === 1) {
+      setTriger(1);
+    }
+  }, [list.length]);
+
   return (
     allOfIngredients && (
       <InngredientsWrapper>
+        {triger === 1 && <MotivatedModal type="first shopping" isOpen={true} />}
         {ingredients.map((inngredient, index) => (
           <RecipeInngredientsItem
             image={inngredient.imgURL}
