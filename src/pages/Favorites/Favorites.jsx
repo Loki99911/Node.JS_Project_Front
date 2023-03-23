5f8aa25afb9e3c67fbea507b035052c63c551627
Iuliia Sokol <avare779@gmail.com>
2023-03-23 10:38:02 +0200

    added loader for recipes fetching

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   getFavoriteRecipes,
+  getIsOwnRecipesFetching,
   getTotalFavoriteRecipes,
 } from 'redux/ownRecipes/ownRecipesSelectors';
 import { getFavorite } from 'redux/ownRecipes/ownRecipesOperations';
@@ -10,17 +11,20 @@ import { Container } from 'components/Container/Container';
 import { Title } from 'components/Title/Title';
 import { RecipeBlock } from 'components/RecipeBlock/RecipeBlock';
 import { PaginationComp } from 'components/PaginationComp/Pagination';
+import { EmptyPagePlug } from 'components/EmptyPagePlug/EmptyPagePlug';
+import { Loader } from 'components/Loader/Loader';
 
 import img from '../../images/default.jpg';
 
 import { ContentWrapper, Wrapper } from './Favorites.styled';
-import { EmptyPagePlug } from 'components/EmptyPagePlug/EmptyPagePlug';
+
 import { scrollToTop } from 'utils/scrollUp';
 
 const Favorites = () => {
   const dispatch = useDispatch();
   const favorites = useSelector(getFavoriteRecipes);
   const total = useSelector(getTotalFavoriteRecipes);
+  const isFetching = useSelector(getIsOwnRecipesFetching);
   const perPage = 4;
   const [page, setPage] = useState(1);
 
@@ -44,35 +48,42 @@ const Favorites = () => {
     <Wrapper>
       <Container>
         <Title>Favorites</Title>
-        {favorites && favorites.length > 0 ? (
-          <ContentWrapper>
-            {favorites.map(item => {
-              return (
-                <li key={item.idMeal}>
-                  <RecipeBlock
-                    location="favorite"
-                    id={item.idMeal}
-                    img={item.strMealThumb ?? img}
-                    title={item.strMeal ?? 'No name'}
-                    text={
-                      <span>{item.strInstructions ?? 'No description'}</span>
-                    }
-                    time={item.cookingTime ? `${item.cookingTime} min` : ''}
-                  />
-                </li>
-              );
-            })}
-          </ContentWrapper>
+        {isFetching ? (
+          <Loader />
         ) : (
-          <EmptyPagePlug text="You currently don't have any favorite recipes added. Let's add some!" />
-        )}
-
-        {favorites && favorites.length > 0 && (
-          <PaginationComp
-            count={Math.ceil(total / perPage)}
-            page={page}
-            handleChange={handleChange}
-          />
+          <>
+            {favorites && favorites.length > 0 ? (
+              <ContentWrapper>
+                {favorites.map(item => {
+                  return (
+                    <li key={item.idMeal}>
+                      <RecipeBlock
+                        location="favorite"
+                        id={item.idMeal}
+                        img={item.strMealThumb ?? img}
+                        title={item.strMeal ?? 'No name'}
+                        text={
+                          <span>
+                            {item.strInstructions ?? 'No description'}
+                          </span>
+                        }
+                        time={item.cookingTime ? `${item.cookingTime} min` : ''}
+                      />
+                    </li>
+                  );
+                })}
+              </ContentWrapper>
+            ) : (
+              <EmptyPagePlug text="You currently don't have any favorite recipes added. Let's add some!" />
+            )}
+            {favorites && favorites.length > 0 && (
+              <PaginationComp
+                count={Math.ceil(total / perPage)}
+                page={page}
+                handleChange={handleChange}
+              />
+            )}
+          </>
         )}
       </Container>
     </Wrapper>
--- src/pages/MyRecipes/MyRecipes.jsx
+++ src/pages/MyRecipes/MyRecipes.jsx
@@ -1,19 +1,24 @@
-import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
 import {
   getCustomRecipes,
+  getIsOwnRecipesFetching,
   getTotalCustomRecipes,
 } from 'redux/ownRecipes/ownRecipesSelectors';
 import { getOwnRecipes } from 'redux/ownRecipes/ownRecipesOperations';
 import { Container } from 'components/Container/Container';
 import { RecipeBlock } from 'components/RecipeBlock/RecipeBlock';
+import { PaginationComp } from 'components/PaginationComp/Pagination';
+import { EmptyPagePlug } from 'components/EmptyPagePlug/EmptyPagePlug';
 import { Title } from 'components/Title/Title';
+import { Loader } from 'components/Loader/Loader';
+
 import { ContentWrapper, Wrapper } from './MyRecipes.styled';
 
 import img from '../../images/default.jpg';
-import { PaginationComp } from 'components/PaginationComp/Pagination';
-import { EmptyPagePlug } from 'components/EmptyPagePlug/EmptyPagePlug';
+
 import { scrollToTop } from 'utils/scrollUp';
 
 const MyRecipes = () => {
@@ -23,6 +28,7 @@ const MyRecipes = () => {
   const page = search.slice(-1);
   const recipes = useSelector(getCustomRecipes);
   const total = useSelector(getTotalCustomRecipes);
+  const isFetching = useSelector(getIsOwnRecipesFetching);
   const perPage = 4;
   const [pageNumber, setPageNumber] = useState(+page);
 
@@ -50,36 +56,42 @@ const MyRecipes = () => {
     <Wrapper>
       <Container>
         <Title>My recipes</Title>
-        {recipes && recipes.length > 0 ? (
-          <ContentWrapper>
-            {recipes.map(item => {
-              return (
-                <li key={item._id}>
-                  <RecipeBlock
-                    location="recipes"
-                    id={item._id}
-                    img={item.imgURL ?? img}
-                    title={item.title ?? 'No name'}
-                    text={
-                      <span>
-                        {item.about ?? item.description ?? 'No description'}
-                      </span>
-                    }
-                    time={item.cookingTime ? `${item.cookingTime} min` : ''}
-                  />
-                </li>
-              );
-            })}
-          </ContentWrapper>
+        {isFetching ? (
+          <Loader />
         ) : (
-          <EmptyPagePlug text="You currently don't have any own recipes added. Let's add some!" />
-        )}
-        {recipes && recipes.length > 0 && (
-          <PaginationComp
-            count={Math.ceil(total / perPage)}
-            page={pageNumber}
-            handleChange={handleChange}
-          />
+          <>
+            {recipes && recipes.length > 0 ? (
+              <ContentWrapper>
+                {recipes.map(item => {
+                  return (
+                    <li key={item._id}>
+                      <RecipeBlock
+                        location="recipes"
+                        id={item._id}
+                        img={item.imgURL ?? img}
+                        title={item.title ?? 'No name'}
+                        text={
+                          <span>
+                            {item.about ?? item.description ?? 'No description'}
+                          </span>
+                        }
+                        time={item.cookingTime ? `${item.cookingTime} min` : ''}
+                      />
+                    </li>
+                  );
+                })}
+              </ContentWrapper>
+            ) : (
+              <EmptyPagePlug text="You currently don't have any own recipes added. Let's add some!" />
+            )}
+            {recipes && recipes.length > 0 && (
+              <PaginationComp
+                count={Math.ceil(total / perPage)}
+                page={pageNumber}
+                handleChange={handleChange}
+              />
+            )}
+          </>
         )}
       </Container>
     </Wrapper>
